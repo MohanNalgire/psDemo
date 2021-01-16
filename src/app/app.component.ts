6614ec78b23288f02a9dfeed05dc34b56199c98d
Mohan Nalgire <mohan.nalgire@ibm.com>
2021-01-16 19:31:01 +0530

    spacex service fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SpacexService } from './spacex.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sp';
+export class AppComponent implements OnInit, OnDestroy {
+  spaceXLoches = [];
+  years: number[];
+  constructor(private spacexService: SpacexService) {}
+
+  range = (start, end, length = end - start + 1) => {
+    return Array.from({ length }, (_, i) => start + i);
+  };
+
+  fetchData(): void {
+    this.spacexService.getSpacexLaunches().subscribe(
+      (data: any) => {
+        this.spaceXLoches = data;
+        console.log('space x data', data);
+      },
+      (error) => {
+        console.error('space x data fetch error:', error);
+      },
+      () => {
+        console.info('space x data fetch completed');
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.fetchData();
+    this.years = this.range(2000, 2020);
+    console.log('this.years', this.years);
+  }
+
+  ngOnDestroy() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
