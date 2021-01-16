21d0b76c554f1a39abd28b7b37a99cc4a18b91c4
Mohan Nalgire <mohan.nalgire@ibm.com>
2021-01-17 02:38:41 +0530

    updated filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SpacexService } from './spacex.service';
+
+interface filtersI {
+  year: number;
+  launch_success: boolean;
+  landing_success: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +14,14 @@ import { SpacexService } from './spacex.service';
 export class AppComponent implements OnInit, OnDestroy {
   spaceXLoches = [];
   years: number[];
+  launchData: any;
+  launchStatus = ['yes', 'no'];
+  filteredLaunchResult: any[];
+  emptyFilter: filtersI = {
+    year: null,
+    launch_success: null,
+    landing_success: null,
+  };
   constructor(private spacexService: SpacexService) {}
 
   range = (start, end, length = end - start + 1) => {
@@ -18,6 +32,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this.spacexService.getSpacexLaunches().subscribe(
       (data: any) => {
         this.spaceXLoches = data;
+        this.selectedFilters(this.emptyFilter);
         console.log('space x data', data);
       },
       (error) => {
@@ -27,6 +42,27 @@ export class AppComponent implements OnInit, OnDestroy {
         console.info('space x data fetch completed');
       }
     );
+  }
+
+  selectedFilters(filter) {
+    const currentFilter: filtersI = {
+      year: filter.year,
+      launch_success: filter.launch_success === 'yes' ? true : false,
+      landing_success: filter.landing_success === 'yes' ? true : false,
+    };
+
+    this.filteredLaunchResult = this.spaceXLoches.filter((e) => {
+      if (currentFilter.year > 0) {
+        return e.launch_year === currentFilter.year.toString();
+      } else if (filter.launch_success) {
+        return e.launch_success === currentFilter.launch_success;
+      } else if (filter.landing_success) {
+        return e.landing_success === currentFilter.landing_success;
+      } else {
+        return this.spaceXLoches;
+      }
+    });
+    return this.filteredLaunchResult;
   }
 
   ngOnInit() {
